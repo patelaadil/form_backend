51d825dab34c78c2bc08cf834c1bb0fc02dad348
Aadil Patel <aadilpatel27042006@example.com>
2025-12-16 22:54:01 +0530

    Backend with Express

---
+++ server.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+require("dotenv").config();
+
+// Create express object 
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Mongoose connection
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("Mongodb Atlas Connected !"))
+.catch((err) => console.log(err));
+
+// Mongoose user model ( create user collection schema )
+
+const Schema = new mongoose.Schema({
+    name:String,
+    email:{type:String, unique:true},
+    password:String
+})
+
+const Emp = mongoose.model("Employee",Schema);
+
+// Register API
+
+app.post("/register", async (req,res) => {
+    const { name , email, password } = req.body;
+
+    // check user exist or not
+    const existUser = await Emp.findOne({ email });
+
+    if(existUser){
+        return res.json({ message: "!!! User already exists !!!" });
+    }
+
+    const hashPassword = await bcrypt.hash(password, 8);
+
+    const emp = new Emp({
+        name,
+        email,
+        password: hashPassword
+    })
+
+    await emp.save();
+
+    res.json({ message: "Registered Successfully !"});
+})
+
+// Login API
+
+app.post("/login", async (req,res) => {
+    const { email, password } = req.body;
+
+    // check user exist or not
+    const existUser = await Emp.findOne({ email });
+
+    if(!existUser){
+        return res.json({ message : "!!! User not exist !!!"});
+    }
+
+    const Match = await bcrypt.compare(password, existUser.password);
+    
+    if(!Match){
+        return res.json({ message : "!!! Password not matched !!!" });
+    }
+
+    res.json({ message : "Successfully Login " });
+})
+
+app.listen(5000, () => {
+    console.log("Server running at http://localhost:5000/");
+})
