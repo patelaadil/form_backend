03331993edd12f8b6ede08136192c8e7f09ad9c6
Aadil Patel <aadilpatel27042006@example.com>
2025-12-17 08:58:35 +0530

    Update port

--- server.js
+++ server.js
@@ -75,6 +75,8 @@ app.post("/login", async (req,res) => {
     res.json({ message : "Successfully Login " });
 })
 
-app.listen(5000, () => {
-    console.log("Server running at https://form-backend-aoaq.onrender.com/");
-})
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
